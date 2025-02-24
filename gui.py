68a88575aa5d859fda1b05899e7a999a28024768
phutanle9 <135482723+phutanle9@users.noreply.github.com>
2025-02-24 15:15:34 +0700

    complete error

--- gui.py
+++ gui.py
@@ -1,6 +1,13 @@
+import time
+
 import FreeSimpleGUI as sg
+from scipy.constants import value
+import time
 import functions
 
+sg.theme("DarkPurple4")
+
+clock = sg.Text('', key='clock')
 # Rename 'label' to avoid conflict
 todo_label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a to-do", key='todo')
@@ -14,13 +21,16 @@ exit_button = sg.Button("Exit")
 
 
 window = sg.Window('My To-do App',
-                   layout=[[todo_label], [input_box, add_button],
-                            [list_box, edit_button,complete_button],
+                   layout=[[clock],
+                           [todo_label],
+                           [input_box, add_button],
+                           [list_box, edit_button,complete_button],
                            [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(1, event)
     print(2, values)
     print(3, values['todos'])
@@ -33,7 +43,7 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)  # Refresh Listbox with updated todos
         case 'Edit':
-            if values['todos']:  # Check if there is a selected todo
+            try:
                 todo_to_edit = values['todos'][0]
                 new_todo = values['todo']
 
@@ -42,6 +52,8 @@ while True:
                 todos[index] = new_todo
                 functions.write_todos(todos)
                 window['todos'].update(values=todos)  # Refresh Listbox with updated todos
+            except IndexError:
+                sg.popup("Please select an item first")
         case "Complete":
             todo_to_complete = values['todos'][0]
             todos = functions.get_todos()
