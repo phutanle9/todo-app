940e7453ca49f9b5c57887f85dda2b83e9d17793
phutanle9 <135482723+phutanle9@users.noreply.github.com>
2025-02-24 15:01:08 +0700

    update complete, exit button

--- gui.py
+++ gui.py
@@ -9,9 +9,14 @@ list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 add_button = sg.Button("Add")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
 
 window = sg.Window('My To-do App',
-                   layout=[[todo_label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[todo_label], [input_box, add_button],
+                            [list_box, edit_button,complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -37,10 +42,21 @@ while True:
                 todos[index] = new_todo
                 functions.write_todos(todos)
                 window['todos'].update(values=todos)  # Refresh Listbox with updated todos
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todos'].update(value='')
+
+        case 'Exit':
+            break
+
         case 'todos':
             if values['todos']:
                 window['todo'].update(value=values['todos'][0])  # Show selected todo in input box
         case sg.WIN_CLOSED:
-            break
+            exit()
 
 window.close()
