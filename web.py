32f124838a9150615a5a77194da91fa36f1937d2
phutanle9 <135482723+phutanle9@users.noreply.github.com>
2025-02-27 01:57:46 +0700

    Add wed gui

--- web.py
+++ web.py
@@ -1,3 +1,17 @@
 import streamlit as st
+from scipy.special import label
+
+import functions
+
+
+todos = functions.get_todos()
+
 
 st.title("My todo App")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input( placeholder="Add new todo...")
